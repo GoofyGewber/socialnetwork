2de9aeed3145219fa5ea5c4d73fe7978c0c98d90
Tom Lawless <tomlaw96@gmail.com>
2013-01-09 19:20:48 -0500

    Tom, you're gay.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomspace::Application.config.secret_token = 'df77db4c2c5b70ea9946608ace6dce03b900a363288f3c75402311e296be512a1888a930050d9e6b6665cf7cfb3e886d3e59f4b531833c3fe99615dbd41424af'
